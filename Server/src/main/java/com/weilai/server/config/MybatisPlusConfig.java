35d7a5a768da3411d1dd0717d62fc03dcb52d431
Wei <1397311537@qq.com>
2022-07-28 18:46:36 +0800

    创建ibatisPlus配置类

---
+++ Server/src/main/java/com/weilai/server/config/MybatisPlusConfig.java
@@ -0,0 +1,13 @@
+package com.weilai.server.config;
+
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@MapperScan("com.weilai.server.mapper")
+public class MybatisPlusConfig {
+
+}
