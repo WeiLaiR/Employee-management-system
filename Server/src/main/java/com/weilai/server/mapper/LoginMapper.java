7beda62559862dec655a7f7a66cbf8d90953f3f0
Wei <1397311537@qq.com>
2022-07-28 18:45:58 +0800

    配置Mapper文件

---
+++ Server/src/main/java/com/weilai/server/mapper/LoginMapper.java
@@ -0,0 +1,9 @@
+package com.weilai.server.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.weilai.server.pojo.Login;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginMapper extends BaseMapper<Login> {
+}
