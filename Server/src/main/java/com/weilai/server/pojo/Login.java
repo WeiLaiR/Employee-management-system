e685edf677cbda6cb2d262a6fe1ea5471677a8ba
Wei <1397311537@qq.com>
2022-07-28 18:45:11 +0800

    创建实体类并配置注解

---
+++ Server/src/main/java/com/weilai/server/pojo/Login.java
@@ -0,0 +1,25 @@
+package com.weilai.server.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Login {
+
+//    ID自动生成，使用雪花算法
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long eid;
+    private String email;
+    private String password;
+//    逻辑删除
+    @TableLogic
+    private Boolean is_delete;
+
+
+}
