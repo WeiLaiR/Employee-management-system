d974538e294dadc53e2a0e957a6ebd2b6098425c
Wei <1397311537@qq.com>
2022-08-01 19:51:20 +0800

    增加部分注解以正确显示时间以及隐藏不应该显示的内容

--- Server/src/main/java/com/weilai/server/pojo/Department.java
+++ Server/src/main/java/com/weilai/server/pojo/Department.java
@@ -3,9 +3,12 @@ package com.weilai.server.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -20,9 +23,14 @@ public class Department {
     private String name;
     private String department;
     private String post;
-    private Date create_time;
-    private Date update_time;
+//    注解优先级高于yml配置文件中的设置！
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
 //    逻辑删除
     @TableLogic
-    private Boolean is_delete;
+//    不展示逻辑删除位
+    @JsonIgnore
+    private Boolean isDelete;
 }
