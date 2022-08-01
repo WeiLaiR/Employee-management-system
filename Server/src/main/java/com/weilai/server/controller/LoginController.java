fa98783f5f5cd5bd3f1235b0bddc77b4b1c322e9
Wei <1397311537@qq.com>
2022-08-01 10:59:46 +0800

    修改

--- Server/src/main/java/com/weilai/server/controller/LoginController.java
+++ Server/src/main/java/com/weilai/server/controller/LoginController.java
@@ -1,19 +1,30 @@
 package com.weilai.server.controller;
 
 
-import com.weilai.server.service.LoginServiceImpl;
+import com.weilai.server.pojo.Login;
+import com.weilai.server.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
 
     @Autowired
-    private LoginServiceImpl loginService;
+    private LoginService loginService;
 
+//    @RequestBody 用于接受前端传过来的json数据
 
+    @GetMapping("/list")
+    public List<Login> LoginList(){
+        return loginService.list();
+    }
+
+    @PostMapping("/save")
+    public boolean LoginSave(@RequestBody Login login){
+        return loginService.save(login);
+    }
 
 }
