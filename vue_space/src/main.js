aa76bb77835706b957ac6595b5640c088f5b6ba5
Wei <1397311537@qq.com>
2022-08-01 19:48:15 +0800

    完成分页查询数据展示，使用axios通讯

--- vue_space/src/main.js
+++ vue_space/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/gloable.css'
@@ -8,6 +10,7 @@ import './assets/gloable.css'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI,{size: "mini"})
+Vue.use(VueAxios,axios)
 
 new Vue({
   router,
