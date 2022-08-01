86999162b9e5a19cb370881cff83df0ad02fd46a
Wei <1397311537@qq.com>
2022-08-01 19:52:59 +0800

    已实现分页查询功能

--- Server/src/main/java/com/weilai/server/controller/DepartmentController.java
+++ Server/src/main/java/com/weilai/server/controller/DepartmentController.java
@@ -1,15 +1,19 @@
 package com.weilai.server.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weilai.server.pojo.Department;
 import com.weilai.server.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 @RestController
 @RequestMapping("/department")
@@ -19,12 +23,12 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     @GetMapping("/getPage/{pageNum}/{pageSize}")
-    public Map<String,Object> getPage(@PathVariable("pageNum") Integer pageNum, @PathVariable("pageSize") Integer pageSize){
+    public Map<String, Object> getPage(@PathVariable("pageNum") Integer pageNum, @PathVariable("pageSize") Integer pageSize) throws JsonProcessingException {
         Page<Department> page = new Page<>(pageNum, pageSize);
         departmentService.page(page);
         HashMap<String, Object> map = new HashMap<>();
-        map.put("pages",page.getPages());
-        map.put("data",page.getRecords());
+        map.put("total",page.getTotal());
+        map.put("values",page.getRecords());
         return map;
     }
 
