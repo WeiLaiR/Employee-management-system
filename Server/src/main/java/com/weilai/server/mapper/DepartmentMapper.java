c98ba0a82b3a57a2edaa182bda7e879ba8f7a584
Wei <1397311537@qq.com>
2022-08-01 11:00:52 +0800

    编写注册Mapper

---
+++ Server/src/main/java/com/weilai/server/mapper/DepartmentMapper.java
@@ -0,0 +1,9 @@
+package com.weilai.server.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.weilai.server.pojo.Department;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentMapper extends BaseMapper<Department> {
+}
