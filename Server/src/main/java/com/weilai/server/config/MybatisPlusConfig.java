8ff0890c518ddad2ec5ceb2b0c039389a4adcee1
Wei <1397311537@qq.com>
2022-08-01 10:59:32 +0800

    ibatisPlus注册分页查询插件

--- Server/src/main/java/com/weilai/server/config/MybatisPlusConfig.java
+++ Server/src/main/java/com/weilai/server/config/MybatisPlusConfig.java
@@ -1,6 +1,9 @@
 package com.weilai.server.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,5 +12,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @MapperScan("com.weilai.server.mapper")
 public class MybatisPlusConfig {
+
+//    配置分页插件
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
 
 }
