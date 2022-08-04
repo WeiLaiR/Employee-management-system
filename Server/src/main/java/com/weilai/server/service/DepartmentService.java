1008587e13e5950b91f28f9482fee32131eb6488
Wei <1397311537@qq.com>
2022-08-04 16:48:44 +0800

    增加addOrUpdateDep接口和方法，将增加和修改设计在同一模块内

--- Server/src/main/java/com/weilai/server/service/DepartmentService.java
+++ Server/src/main/java/com/weilai/server/service/DepartmentService.java
@@ -4,4 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.weilai.server.pojo.Department;
 
 public interface DepartmentService extends IService<Department> {
+
+    boolean addOrUpdateDep(Department department);
 }
--- Server/src/main/java/com/weilai/server/service/DepartmentServiceImpl.java
+++ Server/src/main/java/com/weilai/server/service/DepartmentServiceImpl.java
@@ -3,8 +3,19 @@ package com.weilai.server.service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.weilai.server.mapper.DepartmentMapper;
 import com.weilai.server.pojo.Department;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DepartmentServiceImpl extends ServiceImpl<DepartmentMapper, Department> implements DepartmentService {
+
+    @Autowired
+    private DepartmentMapper departmentMapper;
+    @Override
+    public boolean addOrUpdateDep(Department department) {
+        if (department.getEid() == null){
+            return departmentMapper.insert(department) != 0;
+        }
+        return departmentMapper.updateById(department) != 0;
+    }
 }
