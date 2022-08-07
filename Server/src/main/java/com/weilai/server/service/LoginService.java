61fdb9e5af071b87c7e39621e993a038b8121bc3
Wei <1397311537@qq.com>
2022-08-07 12:24:50 +0800

    编写登录接口

--- Server/src/main/java/com/weilai/server/service/LoginService.java
+++ Server/src/main/java/com/weilai/server/service/LoginService.java
@@ -3,5 +3,10 @@ package com.weilai.server.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.weilai.server.pojo.Login;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 public interface LoginService extends IService<Login> {
+
+    Boolean loginEmp(String email,String password) throws NoSuchAlgorithmException, InvalidKeySpecException;
 }
