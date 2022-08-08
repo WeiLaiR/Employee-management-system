a160d3ec712f618f7b30d4f8cb6d790b644b28cb
Wei <1397311537@qq.com>
2022-08-08 18:41:30 +0800

    实现注册接口

--- Server/src/main/java/com/weilai/server/service/LoginServiceImpl.java
+++ Server/src/main/java/com/weilai/server/service/LoginServiceImpl.java
@@ -6,6 +6,7 @@ import com.weilai.server.mapper.LoginMapper;
 import com.weilai.server.pojo.Login;
 import com.weilai.server.utils.RSA;
 import com.weilai.server.utils.RSAUtils;
+import com.weilai.server.utils.TokenUtils;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -36,6 +37,9 @@ public class LoginServiceImpl extends ServiceImpl<LoginMapper, Login> implements
         HashMap<String, Object> map = new HashMap<>();
         if (emp != null){
             if (pw.equals(emp.getPassword())){
+//                获取Token
+                String token = TokenUtils.getToken(emp.getEid(), emp.getPassword());
+                map.put("token",token);
                 map.put("state","Success");
                 map.put("eid",emp.getEid());
                 map.put("level",emp.getLevel());
@@ -44,6 +48,30 @@ public class LoginServiceImpl extends ServiceImpl<LoginMapper, Login> implements
             }
         }else {
             map.put("state","Error");
+        }
+        return map;
+    }
+
+    @Override
+    public Map<String, Object> registerEmp(String email, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        //      获取私钥，解密数据
+        RSAPrivateKey privateKey = RSAUtils.getPrivateKey(rsa.privateKey);
+        String pw = RSAUtils.privateDecrypt(password, privateKey);
+
+        QueryWrapper<Login> wrapper = new QueryWrapper<>();
+        wrapper.eq("email",email);
+        HashMap<String, Object> map = new HashMap<>();
+        if (loginMapper.selectOne(wrapper) != null){
+            map.put("state","ErrorDuplicate");
+        }else {
+            Login login = new Login();
+            login.setEmail(email);
+            login.setPassword(pw);
+            if (loginMapper.insert(login) > 0){
+                map.put("state","Success");
+            }else {
+                map.put("state","Error");
+            }
         }
         return map;
     }
