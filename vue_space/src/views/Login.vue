f48becf175d0d6cb29c2e4b4cca0b8e3141a639a
Wei <1397311537@qq.com>
2022-08-08 18:45:25 +0800

    略微修改提示内容以及跳转路径

--- vue_space/src/views/Login.vue
+++ vue_space/src/views/Login.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="wrapper">
-    <div style="margin: 200px auto; background-color: #fff; width: 350px; height: 300px; padding: 20px; border-radius: 10px">
-      <div style="margin: 20px 0; text-align: center; font-size: 24px"><b>登 录</b></div>
-      <el-form :model="loginEmp" :rules="rules" ref="empForm">
+  <div>
+    <div class="wrapper">
+      <div style="margin: 200px auto; background-color: #fff; width: 350px; height: 300px; padding: 20px; border-radius: 10px">
+        <div style="margin: 20px 0; text-align: center; font-size: 24px"><b>登 录</b></div>
+        <el-form :model="loginEmp" :rules="rules" ref="empForm">
 
-        <el-form-item prop="email">
-          <el-input size="medium" style="margin: 10px 0" prefix-icon="el-icon-user" v-model="loginEmp.email" type="email"></el-input>
-        </el-form-item>
+          <el-form-item prop="email">
+            <el-input placeholder="请输入邮箱" size="medium" style="margin: 10px 0" prefix-icon="el-icon-user" v-model="loginEmp.email" type="email"></el-input>
+          </el-form-item>
 
-        <el-form-item prop="password">
-          <el-input size="medium" style="margin: 10px 0" prefix-icon="el-icon-lock" show-password v-model="loginEmp.password"></el-input>
-        </el-form-item>
+          <el-form-item prop="password">
+            <el-input placeholder="请输入密码" size="medium" style="margin: 10px 0" prefix-icon="el-icon-lock" show-password v-model="loginEmp.password"></el-input>
+          </el-form-item>
 
-        <el-form-item style="margin: 10px 0; text-align: right">
-          <el-button type="primary" size="small"  autocomplete="off" @click="login">登录</el-button>
-          <el-button type="warning" size="small"  autocomplete="off">注册</el-button>
-        </el-form-item>
-      </el-form>
+          <el-form-item style="margin: 10px 0; text-align: right">
+            <el-button type="primary" size="small"  autocomplete="off" @click="login">登录</el-button>
+            <el-button type="warning" size="small"  autocomplete="off" @click="$router.push('/register')">注册</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </div>
+
+
   </div>
+
 </template>
 
 <script>
 import JSEncrypt from "jsencrypt";
 import store from "@/store";
+import request from "@/utils/request";
 
 export default {
   name: "Login",
@@ -53,7 +59,7 @@ export default {
   },
   created() {
     //请求后端发送公钥
-    this.request.post("/RSA/getPublicKey").then(res => {
+    request.post("/RSA/getPublicKey").then(res => {
       this.publicKey = res.publicKey
     })
   },
@@ -74,11 +80,12 @@ export default {
           console.log(this.loginEnc.encryptPW)
           this.loginEnc.email = this.loginEmp.email
 
-          this.request.post("/login/login/", this.loginEnc).then(res => {
+          request.post("/login/login/", this.loginEnc).then(res => {
             console.log(res)
             if (res.state === "Success") {
               localStorage.setItem("eid", res.eid)
               this.$router.push("/")
+              this.$message.success("登陆成功!")
             }else {
               this.$message.error("用户名或密码错误")
             }
