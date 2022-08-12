15ef0dd236dd224d04a61bed6ae5adda9998d64f
Wei <1397311537@qq.com>
2022-08-12 11:30:21 +0800

    更改为使用set方法注入，变更业务逻辑，密码使用sha3_256加密后存入数据库，登陆后将token信息存入redis，新增logout

--- Server/src/main/java/com/weilai/server/service/LoginServiceImpl.java
+++ Server/src/main/java/com/weilai/server/service/LoginServiceImpl.java
@@ -2,58 +2,71 @@ package com.weilai.server.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.weilai.server.exception.CustomException;
 import com.weilai.server.mapper.LoginMapper;
 import com.weilai.server.pojo.Login;
-import com.weilai.server.utils.RSA;
-import com.weilai.server.utils.RSAUtils;
-import com.weilai.server.utils.TokenUtils;
-import lombok.extern.java.Log;
+import com.weilai.server.utils.*;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
-import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
-import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class LoginServiceImpl extends ServiceImpl<LoginMapper, Login> implements LoginService {
 
-    @Autowired
-    private LoginMapper loginMapper;
+//    设置token过期时间，单位为秒，当前为3天
+    private static final long TIMEOUT = 259200;
 
+    //    更改为使用set方法注入
+    private LoginMapper loginMapper;
+    @Autowired
+    public void setLoginMapper(LoginMapper loginMapper){
+        this.loginMapper = loginMapper;
+    }
+
+
+    private RedisUtil redisUtil;
+    @Autowired
+    public void setRedisUtil(RedisUtil redisUtil) {
+        this.redisUtil = redisUtil;
+    }
     RSA rsa = new RSA();
 
+//    没有使用Bcrypt
+//    使用的是SHA3-256
+
     @Override
-    public Map<String, Object> loginEmp(String email, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    public Map<String, Object> loginEmp(String email, String password) throws Exception {
 //      获取私钥，解密数据
         RSAPrivateKey privateKey = RSAUtils.getPrivateKey(rsa.privateKey);
         String pw = RSAUtils.privateDecrypt(password, privateKey);
-
+        HashMap<String, Object> map = new HashMap<>();
         QueryWrapper<Login> wrapper = new QueryWrapper<>();
         wrapper.eq("email",email);
         Login emp = loginMapper.selectOne(wrapper);
-        HashMap<String, Object> map = new HashMap<>();
+
         if (emp != null){
-            if (pw.equals(emp.getPassword())){
-//                获取Token
-                String token = TokenUtils.getToken(emp.getEid(), emp.getPassword());
+            if (DigestUtils.sha3_256Hex(pw).equals(emp.getPassword())){
+                String token = TokenUtils.getToken(emp.getEid(), emp.getEmail());
+                redisUtil.set(emp.getEid().toString(), token,TIMEOUT);
                 map.put("token",token);
                 map.put("state","Success");
-                map.put("eid",emp.getEid());
-                map.put("level",emp.getLevel());
             }else {
-                map.put("state","Error");
+                throw new CustomException("ErrorLogin","用户名或密码错误！");
             }
         }else {
-            map.put("state","Error");
+            throw new CustomException("ErrorLogin","用户名或密码错误");
         }
         return map;
     }
 
     @Override
-    public Map<String, Object> registerEmp(String email, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    public Map<String, Object> registerEmp(String email, String password) throws Exception {
         //      获取私钥，解密数据
         RSAPrivateKey privateKey = RSAUtils.getPrivateKey(rsa.privateKey);
         String pw = RSAUtils.privateDecrypt(password, privateKey);
@@ -62,17 +75,30 @@ public class LoginServiceImpl extends ServiceImpl<LoginMapper, Login> implements
         wrapper.eq("email",email);
         HashMap<String, Object> map = new HashMap<>();
         if (loginMapper.selectOne(wrapper) != null){
-            map.put("state","ErrorDuplicate");
+            throw new CustomException("ErrorRegister", "该邮箱已被注册！");
         }else {
             Login login = new Login();
             login.setEmail(email);
-            login.setPassword(pw);
+            login.setPassword(DigestUtils.sha3_256Hex(pw));
             if (loginMapper.insert(login) > 0){
                 map.put("state","Success");
             }else {
-                map.put("state","Error");
+                throw new CustomException("ErrorRegister","注册失败，出现未知错误！");
             }
         }
         return map;
+    }
+
+    @Override
+    public Boolean logout() {
+        String eid = Objects.requireNonNull(TokenUtils.getId()).toString();
+
+        if (!StringUtils.hasText(eid)) {
+            throw new CustomException("Error","注销失败，出现未知错误！");
+        }
+        redisUtil.del(eid);
+        redisUtil.del(eid.concat("PW"));
+        System.out.println("===================================注销成功=================================");
+        return true;
     }
 }
