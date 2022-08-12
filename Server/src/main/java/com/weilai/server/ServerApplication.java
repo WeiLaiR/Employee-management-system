03429da8df08600a8743f0ece5cf3519972adae8
Wei <1397311537@qq.com>
2022-08-12 16:15:26 +0800

    增加定时任务注解

--- Server/src/main/java/com/weilai/server/ServerApplication.java
+++ Server/src/main/java/com/weilai/server/ServerApplication.java
@@ -3,11 +3,12 @@ package com.weilai.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
+@EnableAsync //开启异步
+@EnableScheduling //开启定时任务
 @SpringBootApplication
 public class ServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
