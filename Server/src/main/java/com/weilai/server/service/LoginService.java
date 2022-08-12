2ea631b5f7fc66e80d0a2d6b4dc03f36f21504b3
Wei <1397311537@qq.com>
2022-08-12 11:31:24 +0800

    新增logout

--- Server/src/main/java/com/weilai/server/service/LoginService.java
+++ Server/src/main/java/com/weilai/server/service/LoginService.java
@@ -2,15 +2,14 @@ package com.weilai.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.weilai.server.pojo.Login;
-
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 import java.util.Map;
 
 public interface LoginService extends IService<Login> {
 
-    Map<String,Object> loginEmp(String email, String password) throws NoSuchAlgorithmException, InvalidKeySpecException;
+    Map<String,Object> loginEmp(String email, String password) throws Exception;
 
 
-    Map<String,Object> registerEmp(String email,String password) throws NoSuchAlgorithmException, InvalidKeySpecException;
+    Map<String,Object> registerEmp(String email,String password) throws Exception;
+
+    Boolean logout();
 }
