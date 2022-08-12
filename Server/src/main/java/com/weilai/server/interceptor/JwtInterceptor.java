3a4ef67d907d1c5948b47ef868471fb1ca9cd944
Wei <1397311537@qq.com>
2022-08-12 11:36:15 +0800

    新增自定义拦截器，用于校验token

---
+++ Server/src/main/java/com/weilai/server/interceptor/JwtInterceptor.java
@@ -0,0 +1,55 @@
+package com.weilai.server.interceptor;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.weilai.server.exception.CustomException;
+import com.weilai.server.utils.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JwtInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+//        不是映射到方法，直接通过
+        if (!(handler instanceof HandlerMethod)){
+            return true;
+        }
+
+//      校验token是否为空
+        String token = request.getHeader("token");
+        if (!StringUtils.hasText(token)) {
+            throw new CustomException("Error", "无有效信息，请重新登录！");
+        }
+
+//      校验token是否能正常解析
+        String eid;
+        try {
+            eid = JWT.decode(token).getAudience().get(0);
+        }catch (JWTDecodeException jwtDecodeException){
+            throw new CustomException("Error","信息异常，请重新登录！");
+        }
+
+//      是否在redis中有记录
+        String storedToken = redisUtil.get(eid);
+        if (!StringUtils.hasText(storedToken)) {
+            throw new CustomException("Error","信息异常，请重新登录！");
+        }
+
+//      对比token是否一致
+        if (!storedToken.equals(token)) {
+            throw new CustomException("Error","信息异常，请重新登录！");
+        }
+
+        return true;
+    }
+}
