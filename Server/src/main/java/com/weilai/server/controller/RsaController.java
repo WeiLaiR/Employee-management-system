520360cd6e3006bd478dc23d6a5eb6312359d92c
Wei <1397311537@qq.com>
2022-08-12 16:15:56 +0800

    更改获取公钥方式

--- Server/src/main/java/com/weilai/server/controller/RsaController.java
+++ Server/src/main/java/com/weilai/server/controller/RsaController.java
@@ -1,8 +1,8 @@
 package com.weilai.server.controller;
 
 import com.weilai.server.utils.RSA;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +12,16 @@ import java.util.Map;
 @RestController
 @RequestMapping("/RSA")
 public class RsaController {
+    private RSA rsa;
+    @Autowired
+    public void setRsa(RSA rsa) {
+        this.rsa = rsa;
+    }
 
     @PostMapping("/getPublicKey")
     public Map<String,Object> getPublicKey(){
         HashMap<String, Object> map = new HashMap<>();
-        RSA rsa = new RSA();
-        map.put("publicKey", rsa.publicKey);
+        map.put("publicKey", rsa.getPublicKey());
         return map;
     }
 
