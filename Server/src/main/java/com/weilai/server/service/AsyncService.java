fa23b8ed9d308861b9c3d8a5243e79e6a06d08e9
Wei <1397311537@qq.com>
2022-08-15 15:58:06 +0800

    增加异步请求

--- Server/src/main/java/com/weilai/server/service/AsyncService.java
+++ Server/src/main/java/com/weilai/server/service/AsyncService.java
@@ -1,9 +1,12 @@
 package com.weilai.server.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.weilai.server.exception.CustomException;
 import com.weilai.server.mapper.DepartmentMapper;
+import com.weilai.server.mapper.InformationMapper;
 import com.weilai.server.mapper.LoginMapper;
 import com.weilai.server.pojo.Department;
+import com.weilai.server.pojo.Information;
 import com.weilai.server.pojo.Login;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -19,10 +22,15 @@ public class AsyncService {
     }
 
     private DepartmentMapper departmentMapper;
-
     @Autowired
     public void setDepartmentMapper(DepartmentMapper departmentMapper) {
         this.departmentMapper = departmentMapper;
+    }
+
+    private InformationMapper informationMapper;
+    @Autowired
+    public void setInformationMapper(InformationMapper informationMapper) {
+        this.informationMapper = informationMapper;
     }
 
     @Async
@@ -31,14 +39,25 @@ public class AsyncService {
         wrapper.eq("email",email);
         Login login = loginMapper.selectOne(wrapper);
         if (login == null){
-            return;
+            throw new CustomException("Error","数据初始化失败！");
         }
+        boolean sign = false;
         Department department = new Department();
         department.setEid(login.getEid());
-        departmentMapper.insert(department);
+        if (departmentMapper.insert(department) < 1) {
+            sign = true;
+        }
+        Information information = new Information();
+        information.setEid(login.getEid());
+        if (informationMapper.insert(information) < 1) {
+            sign = true;
+        }
 
 
 
+        if (sign) {
+            throw new CustomException("Error","部分数据初始化失败！");
+        }
     }
 
 
