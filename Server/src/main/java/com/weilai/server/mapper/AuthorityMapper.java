c9a887865097cd5ad520b51eb5891a46ed7e0101
Wei <1397311537@qq.com>
2022-08-21 17:47:22 +0800

    新增AuthorityMapper

---
+++ Server/src/main/java/com/weilai/server/mapper/AuthorityMapper.java
@@ -0,0 +1,9 @@
+package com.weilai.server.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.weilai.server.pojo.Authority;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorityMapper extends BaseMapper<Authority> {
+}
