d20e2434737f69a27d7652f167168eeb3781ec32
Wei <1397311537@qq.com>
2022-08-21 17:46:44 +0800

    删除level字段，同时防止password传出后端

--- Server/src/main/java/com/weilai/server/pojo/Login.java
+++ Server/src/main/java/com/weilai/server/pojo/Login.java
@@ -3,6 +3,7 @@ package com.weilai.server.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,10 +17,11 @@ public class Login {
     @TableId(type = IdType.ASSIGN_ID)
     private Long eid;
     private String email;
+    @JsonIgnore
     private String password;
-    private Integer level;
 //    逻辑删除
     @TableLogic
+    @JsonIgnore
     private Boolean isDelete;
 
 
