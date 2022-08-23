96780e96df169bbed5c8eb237c6d2924e74dcbd9
Wei <1397311537@qq.com>
2022-08-23 19:09:51 +0800

    修改侧边栏，动态显示功能

--- vue_space/src/components/Aside.vue
+++ vue_space/src/components/Aside.vue
@@ -11,36 +11,49 @@
       <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; right: 5px">
       <b style="color: white" v-show="logoTextShow">后台管理系统</b>
     </div>
-    <el-menu-item index="/home">
-      <template slot="title">
-        <i class="el-icon-house"></i>
-        <span slot="title">主页</span>
-      </template>
-    </el-menu-item>
-    <el-submenu index="2">
-      <template slot="title">
-        <i class="el-icon-menu"></i>
-        <span slot="title">公司管理</span>
-      </template>
-      <el-menu-item index="/employee">
-        <i class="el-icon-s-custom"></i>
-        <span slot="title">员工管理</span>
+    <div v-if="THome">
+      <el-menu-item index="/home">
+        <template slot="title">
+          <i class="el-icon-house"></i>
+          <span slot="title">主页</span>
+        </template>
       </el-menu-item>
-    </el-submenu>
+    </div>
 
-    <el-menu-item index="/pInformation">
-      <template slot="title">
-        <i class="el-icon-user"></i>
-        <span slot="title">个人信息</span>
-      </template>
-    </el-menu-item>
+    <div v-if="TCompany">
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-menu"></i>
+          <span slot="title">公司管理</span>
+        </template>
 
-    <el-menu-item index="/authority">
-      <template slot="title">
-        <i class="el-icon-set-up"></i>
-        <span slot="title">权限管理</span>
-      </template>
-    </el-menu-item>
+        <div v-if="TEmployee">
+          <el-menu-item index="/employee">
+            <i class="el-icon-s-custom"></i>
+            <span slot="title">员工管理</span>
+          </el-menu-item>
+        </div>
+
+      </el-submenu>
+    </div>
+
+    <div v-if="TpInformation">
+      <el-menu-item index="/pInformation">
+        <template slot="title">
+          <i class="el-icon-user"></i>
+          <span slot="title">个人信息</span>
+        </template>
+      </el-menu-item>
+    </div>
+
+    <div v-if="TAuthority">
+      <el-menu-item index="/authority">
+        <template slot="title">
+          <i class="el-icon-set-up"></i>
+          <span slot="title">权限管理</span>
+        </template>
+      </el-menu-item>
+    </div>
 
   </el-menu>
 </template>
@@ -51,6 +64,37 @@ export default {
   props: {
     isCollapse: Boolean,
     logoTextShow: Boolean
+  },
+  data () {
+    return {
+      THome: false,
+      TCompany: false,
+      TEmployee: false,
+      TpInformation: false,
+      TAuthority: false,
+      authority: 0
+    }
+  },
+  created() {
+    this.authority = localStorage.getItem("authority")
+    let value = 1
+    let count = 1
+    while (value <= this.authority) {
+      if ((value & this.authority) > 0) {
+        if (count === 1) {
+          this.THome =true
+        }else if (count === 2) {
+          this.TCompany = true
+          this.TEmployee = true
+        }else if (count === 3) {
+          this.TpInformation = true
+        }else if (count === 4) {
+          this.TAuthority = true
+        }
+      }
+      value <<= 1
+      count ++
+    }
   }
 }
 </script>
