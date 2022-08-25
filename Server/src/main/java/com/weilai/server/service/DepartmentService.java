ce0a7f227dba1ef96d6a9802ef118680294c5e4b
Wei <1397311537@qq.com>
2022-08-25 17:40:02 +0800

    新增部分接口

--- Server/src/main/java/com/weilai/server/service/DepartmentService.java
+++ Server/src/main/java/com/weilai/server/service/DepartmentService.java
@@ -3,7 +3,15 @@ package com.weilai.server.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.weilai.server.pojo.Department;
 
+import java.util.Map;
+
 public interface DepartmentService extends IService<Department> {
 
     boolean addOrUpdateDep(Department department);
+
+    Map<String,Object> getPage(Integer pageNum,Integer pageSize,String name,String dep);
+
+    Map<String,Object> getDepById(Long eid);
+
+    Map<String,Object> getDep();
 }
