96f882831508cb081d5d459ac6469b1482f1c7ac
Wei <1397311537@qq.com>
2022-08-26 15:04:55 +0800

    创建打卡Controller并实现打卡和获取某一时间段打卡信息的功能

---
+++ Server/src/main/java/com/weilai/server/controller/ClockInController.java
@@ -0,0 +1,31 @@
+package com.weilai.server.controller;
+
+import com.weilai.server.service.ClockInService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/clock")
+public class ClockInController {
+
+    private ClockInService clockInService;
+    @Autowired
+    public void setClockInService(ClockInService clockInService) {
+        this.clockInService = clockInService;
+    }
+
+    @PostMapping("/set")
+    public Boolean setClockIn() {
+        return clockInService.setClockIn();
+    }
+
+    @GetMapping("/getNowPage/{pageNum}/{pageSize}/{day}")
+    public Map<String,Object> getNowPage(@PathVariable("pageNum") Integer pageNum,
+                                         @PathVariable("pageSize") Integer pageSize,
+                                         @PathVariable("day") Integer day) {
+        return clockInService.getNowClockInPage(pageNum,pageSize,day);
+    }
+
+}
