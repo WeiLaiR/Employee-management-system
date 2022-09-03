357e401242c065adf5a455100bcecf18acf68fb4
Wei <1397311537@qq.com>
2022-09-03 11:20:53 +0800

    将当前模块注册到服务器中心

--- Server/src/main/java/com/weilai/server/ServerApplication.java
+++ Server/src/main/java/com/weilai/server/ServerApplication.java
@@ -2,11 +2,13 @@ package com.weilai.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync //开启异步
 @EnableScheduling //开启定时任务
+@EnableDiscoveryClient //注册到服务器中心
 @SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
