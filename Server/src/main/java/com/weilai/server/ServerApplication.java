79c43cb5198f7cff7402b8ac1b84033118d360f6
Wei <1397311537@qq.com>
2022-09-06 17:54:23 +0800

    开启Feign

--- Server/src/main/java/com/weilai/server/ServerApplication.java
+++ Server/src/main/java/com/weilai/server/ServerApplication.java
@@ -3,12 +3,14 @@ package com.weilai.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync //开启异步
 @EnableScheduling //开启定时任务
 @EnableDiscoveryClient //注册到服务器中心
+@EnableFeignClients //开启Feign
 @SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
