6606509e23dab6066a5784c760f4430649bf8d58
Wei <1397311537@qq.com>
2022-09-07 16:59:58 +0800

    导入测试类

---
+++ Server_rsa/src/test/java/com/weilai/server_rsa/ServerRsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weilai.server_rsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
