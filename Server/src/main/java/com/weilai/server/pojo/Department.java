3f02d3f9ff9fe9b47f648bd692e1e3f492fa1f1b
Wei <1397311537@qq.com>
2022-09-07 16:56:50 +0800

    略微修改

--- Server/src/main/java/com/weilai/server/pojo/Department.java
+++ Server/src/main/java/com/weilai/server/pojo/Department.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
